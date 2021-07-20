f2597ec1f2fc160beead75da4ed61069d748e9ab
rifanid98 <adninsijawa.office@gmail.com>
2021-07-20 11:35:51 +0700

    expanding GET

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,7 +13,30 @@ func main() {
 		c.String(http.StatusOK, "Hello World")
 		return nil
 	})
-	e.Logger.Info("Listening on port 8080")
+	products := []map[int]string{
+		{1: "Laptop"},
+		{2: "Phone"},
+		{3: "Mouse"},
+	}
+	e.GET("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		for _, p := range products {
+			for k := range p {
+				productId, err := strconv.Atoi(c.Param("id"))
+				if err != nil {
+					return err
+				}
+				if productId == k {
+					product = p
+				}
+			}
+		}
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "product not found")
+		}
+		return c.JSON(http.StatusOK, product)
+	})
+	e.Logger.Print("Listening on port 8080")
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
 		e.Logger.Fatal(err)
 	}
