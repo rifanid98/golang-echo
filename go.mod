07f2b8e1cc5bd3c4b5af05ea04ad750300572492
rifanid98 <adninsijawa.office@gmail.com>
2021-07-20 11:27:34 +0700

    starting echo

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module golang-echo
 
 go 1.16
+
+require (
+	github.com/labstack/echo/v4 v4.4.0
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- main.go
+++ main.go
@@ -1,58 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func handlerHelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-		</head>
-		<body>
-			<h1>Hello World</h1>
-		</body>
-		</html>
-	`)
-}
-
-func handlerWelcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-		</head>
-		<body>
-			<h1>Welcome!</h1>
-		</body>
-		</html>
-	`)
-}
-
-func handleMethod(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		fmt.Fprintln(w, "GET Method")
-	case "POST":
-		fmt.Fprintln(w, "POST Method")
-	default:
-		fmt.Fprintln(w, "All Method")
-	}
-}
-
 func main() {
-	http.HandleFunc("/", handlerHelloWorld)
-	http.HandleFunc("/welcome", handlerWelcome)
-	http.HandleFunc("/method", handleMethod)
-	http.ListenAndServe("localhost:8080", nil)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		c.String(http.StatusOK, "Hello World")
+		return nil
+	})
+	e.Logger.Info("Listening on port 8080")
+	if err := e.Start(":8080"); err != http.ErrServerClosed {
+		e.Logger.Fatal(err)
+	}
 }
